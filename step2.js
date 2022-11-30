e544480679731f751c8fcfb3724a660a457dc7c4
joshua <joshua.r.foster3@gmail.com>
2022-11-30 00:57:11 -0500

    complete

---
+++ step2.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const process = require('process');
+const axios = require('axios');
+function cat(path){
+    fs.readFile(path,'utf8', function(err, data) {
+        if (err) {
+            console.error(err)
+            process.exit(1)
+        }
+        console.log(data)
+    })
+};
+
+async function webCat(URL){
+    try{
+        let resp = await axios.get(URL)
+        console.log(resp.data)
+    } catch (err) {
+        console.error(err)
+        process.exit()
+    }
+}
+
+let path = process.argv[2]
+
+if (path.slice(0,4) === 'http') {
+    webCat(path);
+}else {
+    cat(path);
+}
+
+
